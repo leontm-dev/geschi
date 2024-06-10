2a82cd439ceebe9eb145b7697d90ac91a6956f98
Leon Anneken <83974950+leontm-dev@users.noreply.github.com>
2024-06-10 11:38:02 +0200

    Added new eventListeners

--- script.js
+++ script.js
@@ -4,26 +4,15 @@ document.getElementById("picture-merten").addEventListener("click", (ev) => {
 document.getElementById("picture-maarten").addEventListener("click", (ev) => {
   document.getElementById("content-maarten").style.display = "flex";
 });
-document.getR;
-function openContent(content) {
-  switch (content) {
-    case "merten":
-      document.getElementById(`content-${content}`).style.display = "flex";
-      break;
-    case "maarten":
-      document.getElementById("content-maarten").style.display = "flex";
-      break;
-    case "robin":
-      document.getElementById("content-robin").style.display = "flex";
-      break;
-    case "leon":
-      document.getElementById("content-leon").style.display = "flex";
-      break;
-    default:
-      break;
-  }
-  return;
-}
+document.getElementById("picture-leon").addEventListener("click", (ev) => {
+  document.getElementById("content-leon").style.display = "flex";
+});
+document.getElementById("picture-robin").addEventListener("click", (ev) => {
+  document.getElementById("content-robin").style.display = "flex";
+});
+document.getElementById("picture-fazit").addEventListener("click", (ev) => {
+  document.getElementById("content-fazit").style.display = "flex";
+});
 function closeContent(content) {
   switch (content) {
     case "merten":
