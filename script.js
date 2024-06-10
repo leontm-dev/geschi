ef8b63e85d87600c7a4bcff94d8e42391139984e
Leon Anneken <83974950+leontm-dev@users.noreply.github.com>
2024-06-10 11:44:32 +0200

    Readded function

--- script.js
+++ script.js
@@ -13,6 +13,26 @@ document.getElementById("picture-robin").addEventListener("click", (ev) => {
 document.getElementById("picture-fazit").addEventListener("click", (ev) => {
   document.getElementById("content-fazit").style.display = "flex";
 });
+
+function openContent(content) {
+  switch (content) {
+    case "merten":
+      document.getElementById(`content-${content}`).style.display = "flex";
+      break;
+    case "maarten":
+      document.getElementById("content-maarten").style.display = "flex";
+      break;
+    case "robin":
+      document.getElementById("content-robin").style.display = "flex";
+      break;
+    case "leon":
+      document.getElementById("content-leon").style.display = "flex";
+      break;
+    default:
+      break;
+  }
+  return;
+}
 function closeContent(content) {
   switch (content) {
     case "merten":
